4eca9af66dc733f0c3e9b11ff4260725d78035df
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-26 12:43:59 +0530

    with practice

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+//car--->Sub class
+//vehicle--->Super class
+public class Car extends Vehicle {
+
+    int wheels;
+    int noOfGears;
+    boolean isHavePowerMirror;
+    
+    
+    public void turnOnWiper() {}
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+//Inheritance
+
+public class Vehicle {
+
+    int wheels;
+    int noOFGears;
+    boolean isHavePowerMirror;
+
+
+    public void turnOnWiper(){}
+}
